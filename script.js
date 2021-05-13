225e6b5792c4f16bb28318e35a83fceeae476155
Lex95 <andrea.lega1995@gmail.com>
2021-05-13 15:47:25 +0200

    aggiunta axios funzionante

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const app = new Vue({
     el: "#app",
     data: {
-
+        key: "c5d3024d8abad31949172938951b96d9",
+        query: "",
+        moviesList: [],
     },
     methods: {
-        
+        querySubmit() {
+            axios.get("https://api.themoviedb.org/3/search/movie", {
+                params: {
+                    api_key: this.key,
+                    query: this.query
+                }
+            }).then((resp) => {
+                this.moviesList = resp.data.results
+            })
+        }
     }
 })
